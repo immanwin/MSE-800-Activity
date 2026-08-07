7ae0a2e18867def923c91c3da89d869e1e9b7869
Immanuel Abraham <immanwin@gmail.com>
2026-08-08 07:59:36 +1200

    Add files via upload

--- Week-1/BMI_Calculator.py
+++ Week-1/BMI_Calculator.py
@@ -1,10 +1,11 @@
 #Problem-2: BMI Calculator
+def main():
+    height = float(input("Height (m)= "))
+    weight = float(input("Weight (Kg)= "))
+    BMI_Calculator (weight,height)
+def BMI_Calculator(weight,height):
+    bmi = weight/(height**2)
+    print(f"Body-Mass-Index = {bmi:.2f} m/kg^2")
 
-height = float(input("Height (m)= "))
-weight = float(input("Weight (Kg)= "))
-
-bmi = weight/(height**2)
-
-print(f"Height: {height}m")
-print(f"Weight: {weight}Kg")
-print(f"Body-Mass-Index = {bmi:.2f} m/kg^2")
+if __name__ == "__main__":
+    main()
