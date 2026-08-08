55896adb2e1d438781cfcf32b27bc6bdbac9922c
Immanuel Abraham <immanwin@gmail.com>
2026-08-09 09:48:43 +1200

    Add files via upload

---
+++ Week-2/list_activity.py
@@ -0,0 +1,12 @@
+my_list = [1,3,5,7,9,11]
+my_list [2:4] = [-3,-9,-11,-13]
+
+print(my_list)
+
+new_list = [1,3,5]
+new_list [1:1] = [-3,-9]
+print(new_list)
+
+example_list = [1,2,3,4,5,6]
+example_list [2:2] = [6,11,27,23,27,17,29]
+print(example_list)
