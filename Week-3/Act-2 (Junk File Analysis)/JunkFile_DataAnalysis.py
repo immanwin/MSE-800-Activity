00da2d11caacfc3214dbfc4262833092f0ed4989
Immanuel Abraham <immanwin@gmail.com>
2026-08-16 09:11:23 +1200

    Update JunkFile_DataAnalysis.py

--- Week-3/Act-2 (Junk File Analysis)/JunkFile_DataAnalysis.py
+++ Week-3/Act-2 (Junk File Analysis)/JunkFile_DataAnalysis.py
@@ -1,19 +1,25 @@
+#Importing pathlib for easier location of file
 from pathlib import Path
 
 file_path = Path(__file__).resolve().parent / "junk.txt"
 
+
 with open(file_path,"r") as junk:
+    #Reading total lines and Counting the no of lines
     total_lines = junk.readlines()
     print(f"No of Lines: {len(total_lines)}")
 
 with open(file_path,"a") as junk:
+    #Appending a new line through Append mode
     new_line = "text file nanalyssis"
     junk.write(new_line)
     print("--> New Line Added...")
 
 with open(file_path,"r") as junk:
+    #Copying file content instead of overwriting
     lowercase_junk = junk.read().lower()
 
 with open(file_path,"w") as junk:
+    #Changing the file content into Lowercase
     junk.write(lowercase_junk)
     print("--> Changed to Lowercase...")
